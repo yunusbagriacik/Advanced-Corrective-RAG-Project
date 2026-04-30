a4338ac175b7d56de6c7943cce36484932e235aa
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-04-30 22:42:45 +0300

    router has just been updated with some testing

--- graph/chains/router.py
+++ graph/chains/router.py
@@ -3,6 +3,9 @@ from typing import Literal #verdiğimiz yapının herhangi birini seçme özelli
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class RouteQuery(BaseModel):
@@ -14,8 +17,8 @@ class RouteQuery(BaseModel):
     )
 
 
-llm = ChatOpenAI(temperature=0)
-structured_llm_router = llm.with_structured_output(RouteQuery)
+llm = ChatOpenAI(temperature=0) #yaratıcı olmasını istemediğimiz için 0 verdik.
+structured_llm_router = llm.with_structured_output(RouteQuery) #llm çıktısını yapısal bir şekilde ele al ve llm ile bağla.
 
 system = """You are an expert at routing a user question to a vectorstore or web search.
 The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
@@ -27,4 +30,9 @@ route_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-question_router = route_prompt | structured_llm_router
+question_router = route_prompt | structured_llm_router # route_promptu al -> structured_llm_router'a ver.
+
+"""
+if __name__ =="__main__":
+    print(question_router.invoke({"question" : "What is Docker?"}))
+"""
