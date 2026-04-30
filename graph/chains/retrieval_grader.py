1a3195ffec0e1fa17d4ace6aa2557d825bd4d099
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-05-01 00:13:45 +0300

    retrieval grader has been created

---
+++ graph/chains/retrieval_grader.py
@@ -0,0 +1,40 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from ingestion import retriever
+load_dotenv()
+
+llm = ChatOpenAI(temperature=0) #yaratıcı olmasını istemediğimiz için 0 verdik.
+
+
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents."""
+
+    binary_score: str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
+
+
+structured_llm_grader = llm.with_structured_output(GradeDocuments) #llm çıktısını yapısal bir şekilde ele al ve llm ile bağla.
+
+system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
+    If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+grade_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
+    ]
+)
+
+retrieval_grader = grade_prompt | structured_llm_grader
+
+"""
+if __name__ == "__main__":
+    user_question = "What is fireflies?"
+    docs = retriever.get_relevant_documents(user_question)
+    retrieved_document = docs[0].page_content
+    print(retrieval_grader.invoke({"question": user_question, "document": retrieved_document}))
+    
+"""
